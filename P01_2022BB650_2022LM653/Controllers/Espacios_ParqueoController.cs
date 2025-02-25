9c5c310bface381ba172945f37b04ac87f72ff10
walleo503 <walter.linares1@catolica.edu.sv>
2025-02-24 19:47:54 -0600

    nuevo

--- P01_2022BB650_2022LM653/Controllers/Espacios_ParqueoController.cs
+++ P01_2022BB650_2022LM653/Controllers/Espacios_ParqueoController.cs
@@ -16,5 +16,73 @@ namespace P01_2022BB650_2022LM653.Controllers
         {
             _Espacios_ParqueoContexto = Espacios_ParqueoContexto;
         }
+
+        //Ver todas las sucusales.
+        [HttpGet]
+        [Route("GetAll")]
+        public IActionResult Espacio_de_parqueo()
+        {
+            List<Espacios_Parqueo> ListaParqueos = (from e in _Espacios_ParqueoContexto.Espacios_Parqueos
+                                                select e).ToList();
+            if (ListaParqueos.Count == 0)
+            {
+                return NotFound();
+            }
+            return Ok(ListaParqueos);
+        }
+
+        //Agregar nuevo Espacio de parqueo 
+        [HttpPost]
+        [Route("add")]
+        public IActionResult GuardarEspacio_de_Parque([FromBody] Espacios_Parqueo espacio_de_parqueo)
+        {
+            try
+            {
+                _Espacios_ParqueoContexto.Espacios_Parqueos.Add(espacio_de_parqueo);
+                _Espacios_ParqueoContexto.SaveChanges();
+                return Ok(espacio_de_parqueo);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        //Actualizar Espacios de parqueo
+        [HttpPost]
+        [Route("Actualizar/{id}")]
+        public IActionResult ActualizarEspacios_de_paqueo(int id, [FromBody] Espacios_Parqueo espacios_Parqueo_Modificar)
+        {
+            var EspacioActual = _Espacios_ParqueoContexto.Espacios_Parqueos.Find(id);
+
+            if (EspacioActual == null) { return NotFound(); }
+
+            EspacioActual.sucursalId = espacios_Parqueo_Modificar.sucursalId;
+            EspacioActual.Numero = espacios_Parqueo_Modificar.Numero;
+            EspacioActual.Ubicacion = espacios_Parqueo_Modificar.Ubicacion;
+            EspacioActual.Costo_la_hora = espacios_Parqueo_Modificar.Costo_la_hora;
+            EspacioActual.Estado = espacios_Parqueo_Modificar.Estado;
+
+            _Espacios_ParqueoContexto.Entry(EspacioActual).State = EntityState.Modified;
+            _Espacios_ParqueoContexto.SaveChanges();
+            return Ok(espacios_Parqueo_Modificar);
+        }
+
+        //Eliminar espacio de parqueo
+        [HttpDelete]
+        [Route("Eliminar/{id}")]
+        public IActionResult EliminarEspacio_de_parqueo(int id)
+        {
+            var Espacio = _Espacios_ParqueoContexto.Espacios_Parqueos.Find(id);
+
+            if (Espacio == null)
+            {
+                return NotFound();
+            }
+            _Espacios_ParqueoContexto.Remove(Espacio);
+            _Espacios_ParqueoContexto.SaveChanges();
+            return Ok(Espacio);
+        }
+
     }
 }
--- P01_2022BB650_2022LM653/Controllers/SucursalesController.cs
+++ P01_2022BB650_2022LM653/Controllers/SucursalesController.cs
@@ -45,5 +45,45 @@ namespace P01_2022BB650_2022LM653.Controllers
                 return BadRequest(ex.Message);
             }
         }
+
+        //Actualizar sucursal
+        [HttpPost]
+        [Route("Actualizar/{id}")]
+        public IActionResult ActualizarSucursal(int id, [FromBody] Sucursales sucursalmodificar)
+        {
+            var SucursalActual = _SucursalContexto.Sucursales.Find(id);
+
+            if (SucursalActual == null) { return NotFound(); }
+
+            SucursalActual.Nombre_Sucursal = sucursalmodificar.Nombre_Sucursal;
+            SucursalActual.Direccion = sucursalmodificar.Direccion;
+            SucursalActual.Telefono = sucursalmodificar.Telefono;
+            SucursalActual.usuarioId = sucursalmodificar.usuarioId;
+            SucursalActual.Espacios_disponibles = sucursalmodificar.Espacios_disponibles;
+
+            _SucursalContexto.Entry(SucursalActual).State = EntityState.Modified;
+            _SucursalContexto.SaveChanges();
+            return Ok(sucursalmodificar);
+        }
+
+        //Eliminar sucursal 
+        [HttpDelete]
+        [Route("Eliminar/{id}")]
+        public IActionResult EliminarSucursal(int id)
+        {
+            var Sucursal = _SucursalContexto.Sucursales.Find(id);
+
+            if (Sucursal == null)
+            {
+                return NotFound();
+            }
+            _SucursalContexto.Remove(Sucursal);
+            _SucursalContexto.SaveChanges();
+            return Ok(Sucursal);
+        }
+
+        //Registrar nuevo espacio.
+
+
     }
 }
